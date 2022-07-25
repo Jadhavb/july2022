7ceb210f256580f311caa8e79b7039910ab9d9b2
Shree <Shree@DESKTOP-LPN97U9>
2022-07-25 20:56:50 +0530

    3rd commit

--- src/com/java/git/demo/Sample_1.java
+++ src/com/java/git/demo/Sample_1.java
@@ -1,5 +1,12 @@
 package com.java.git.demo;
 
-public class Sample_1 {
-
+public class Sample_1 
+{
+public static void main(String[] args) 
+{
+    int a=10;
+    int b=20;
+    int sum=a+b;
+    System.out.println(sum);
+}
 }
